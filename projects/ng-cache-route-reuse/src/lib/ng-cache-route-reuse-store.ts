5f9e7b4023039dab6e89a70f646a926d4501d507
Stas Amasev <41595972+framasev@users.noreply.github.com>
2020-05-17 21:45:55 +0300

    Create cache-route-reuse store

---
+++ projects/ng-cache-route-reuse/src/lib/ng-cache-route-reuse-store.ts
@@ -0,0 +1,43 @@
+import { Type } from '@angular/core';
+import { DetachedRouteHandle } from '@angular/router';
+
+import { Subject } from 'rxjs';
+
+let instance: NgCacheRouteReuseStore = null;
+
+export class NgCacheRouteReuseStore {
+
+  private readonly store: Map<string | Type<any>, DetachedRouteHandle>;
+
+  public readonly add$: Subject<string | Type<any>>;
+  public readonly remove$: Subject<string | Type<any>>;
+
+  constructor() {
+    if (instance) return instance;
+
+    this.store = new Map();
+    this.add$ = new Subject();
+    this.remove$ = new Subject();
+
+    instance = this;
+  }
+
+  public add(component: string | Type<any>, handle: DetachedRouteHandle): void {
+    this.store.set(component, handle);
+    this.add$.next(component);
+  }
+
+  public remove(component: string | Type<any>): void {
+    this.store.delete(component);
+    this.remove$.next(component);
+  }
+
+  public get(component: string | Type<any>): DetachedRouteHandle | null {
+    return this.store.get(component);
+  }
+
+  public has(component: string | Type<any>): boolean {
+    return this.store.has(component);
+  }
+
+}
