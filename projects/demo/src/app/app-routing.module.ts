adc41c93b58d753a71e4a04c96bb0ab2f30c4931
Stas Amasev <41595972+framasev@users.noreply.github.com>
2020-05-17 12:46:11 +0300

    Define app routes

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RouteOneComponent } from './route-one/route-one.component';
+import { RouteTwoComponent } from './route-two/route-two.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'one',
+    component: RouteOneComponent
+  },
+  {
+    path: 'two',
+    component: RouteTwoComponent
+  },
+  {
+    path: 'lazy-one',
+    loadChildren: () => import('./lazy-route-one/lazy-route-one.module').then(m => m.LazyRouteOneModule)
+  },
+  {
+    path: 'lazy-two',
+    loadChildren: () => import('./lazy-route-two/lazy-route-two.module').then(m => m.LazyRouteTwoModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
