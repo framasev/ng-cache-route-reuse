4eac878e1e47e11a8f1d1c91e7690de1f6350a08
Stas Amasev <41595972+framasev@users.noreply.github.com>
2020-07-12 11:31:31 +0300

    style(strategy): lift global dependencies

--- projects/ng-cache-route-reuse/src/lib/ng-cache-route-reuse.strategy.ts
+++ projects/ng-cache-route-reuse/src/lib/ng-cache-route-reuse.strategy.ts
@@ -1,11 +1,7 @@
-import {
-  RouteReuseStrategy,
-  ActivatedRouteSnapshot,
-  DetachedRouteHandle
-} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
 
 import { NgCacheRouteReuseStore } from './ng-cache-route-reuse-store';
-import { Injectable } from '@angular/core';
 
 @Injectable()
 export class NgCacheRouteReuseStrategy implements RouteReuseStrategy {
