20893c88b429ec896856cd84ad31f28925723a0d
Stas Amasev <41595972+framasev@users.noreply.github.com>
2020-10-11 21:31:18 +0300

    docs(ng-cache-route-reuse.service): jsdoc description

--- projects/ng-cache-route-reuse/src/lib/ng-cache-route-reuse.service.ts
+++ projects/ng-cache-route-reuse/src/lib/ng-cache-route-reuse.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Type } from '@angular/core';
-
 import { Observable } from 'rxjs';
 
 import {
@@ -7,6 +6,10 @@ import {
   StoreAction,
 } from './ng-cache-route-reuse-store.service';
 
+/**
+ * Provides access to reuse route's component attach/detach hooks
+ * and detached routes store
+ */
 @Injectable()
 export class NgCacheRouteReuseService {
   private static instance: NgCacheRouteReuseService = null;
@@ -24,26 +27,45 @@ export class NgCacheRouteReuseService {
     return NgCacheRouteReuseService.instance;
   }
 
+  /**
+   * `Attach` component lifecycle hook.
+   *
+   * Triggers on component reattach - re-navigate on reuse route.
+   */
   public onAttach(
     component: string | Type<any>
   ): Observable<string | Type<any>> {
     return this.storeService.on(StoreAction.Delete, component);
   }
 
+  /**
+   * `Detach` component lifecycle hook.
+   *
+   * Triggers on component detach - navigate from reuse route.
+   */
   public onDetach(
     component: string | Type<any>
   ): Observable<string | Type<any>> {
     return this.storeService.on(StoreAction.Set, component);
   }
 
+  /**
+   * Check whether component is attached
+   */
   public isAttached(component: string | Type<any>): boolean {
     return !this.storeService.has(component);
   }
 
+  /**
+   * Check whether component is detached
+   */
   public isDetached(component: string | Type<any>): boolean {
     return this.storeService.has(component);
   }
 
+  /**
+   * Delete all detached routes
+   */
   public clear(): void {
     this.storeService.clear();
   }
